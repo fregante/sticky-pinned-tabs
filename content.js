ccc6c8166f8bbc80470f706fa75e76b0b854c903
Federico Brigante <bfred-it@users.noreply.github.com>
2016-08-24 09:53:16 +0700

    Use native Element.closest()

--- content.js
+++ content.js
@@ -1,18 +1,7 @@
 'use strict';
 
-function closest(element, selector, checkYoSelf) {
-	let parent = checkYoSelf ? element : element.parentNode;
-
-	while (parent && parent !== document) {
-		if (parent.matches(selector)) {
-			return parent;
-		}
-		parent = parent.parentNode;
-	}
-}
-
 function newTabMaybe(e) {
-	const link = closest(e.target, 'a[href]', true);
+	const link = e.target.closest('a[href]');
 	if (link.href.indexOf(location.protocol + '//' + location.host) < 0) {
 		window.open(link.href);
 		e.preventDefault();
